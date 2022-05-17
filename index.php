a187143a870425d343ce1c7bdd204dd509f6e095
BenHassenMarwen <marwen.benhassen.contact@gmail.com>
2022-05-17 22:48:33 +0100

    modification

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Student Management System</title>
+    <title>Iset sfax</title>
     <link rel="stylesheet" type="text/css" href="style.css">
 
     <!-- Latest compiled and minified CSS -->
